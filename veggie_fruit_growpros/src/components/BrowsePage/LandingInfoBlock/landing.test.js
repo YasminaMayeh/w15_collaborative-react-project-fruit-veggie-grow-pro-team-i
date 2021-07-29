698dc203ecd72657f59aef712e9a7af6f0f7a0a4
Luke Browne <l@lukeb.tech>
2021-07-29 16:53:15 +0000

    Rename `.bablerc` to `.babelrc` and fix import to be `./LandingInfoBlock` not `./`

--- veggie_fruit_growpros/src/components/BrowsePage/LandingInfoBlock/landing.test.js
+++ veggie_fruit_growpros/src/components/BrowsePage/LandingInfoBlock/landing.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import LandingInfoBlock from "./";
+import LandingInfoBlock from "./LandingInfoBlock";
 
 const testProps = {
   infoHead: "title-one",
